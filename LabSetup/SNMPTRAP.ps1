768498a381490997e48004b0afd054989340d6c7
Julio Ureña <juliourena33@gmail.com>
2020-03-27 18:03:28 -0400

    Update SNMPTRAP.ps1

--- LabSetup/SNMPTRAP.ps1
+++ LabSetup/SNMPTRAP.ps1
@@ -1,9 +1,8 @@
 $UserName = 'zerotrustws\sqlservice'
-$Service = 'SNMPTRAP' 
-$ServerN = 'SRV-WEB-01'
+$Service = 'SNMPTRAP'
 $Password = 'Supersecure!'
 
-$svcD=gwmi win32_service -computername $ServerN -filter "name='$service'"
+$svcD=gwmi win32_service -filter "name='$service'"
 $svcD | Invoke-WmiMethod -Name ChangeStartMode -ArgumentList "Automatic"
 $svcD.StopService() 
 $svcD.change($null,$null,$null,$null,$null,$null,$UserName,$Password,$null,$null,$null) 
